3587a37abc3ee08412dc5de4c7d0d8a9a10e1e00
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-10 00:09:22 -0500

    fix view employees sql statement
    previously it joined role id with department id
    now it joins role id from role with employee role id

--- index.js
+++ index.js
@@ -72,11 +72,11 @@ function menu(){
 
 function viewEmployees(){
     let sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,
-                CASE  When manager.last_name is not null then CONCAT(manager.first_name, ' ', manager.last_name)
-                    else manager.first_name end as manager
+                CASE  WHEN manager.last_name IS NOT null THEN CONCAT(manager.first_name, ' ', manager.last_name)
+                    ELSE manager.first_name END AS manager
             FROM employee
             LEFT JOIN employee manager ON manager.id = employee.manager_id
-            JOIN role ON role.department_id = employee.role_id
+            JOIN role ON role.id = employee.role_id
             JOIN department ON role.department_id = department.id
             ORDER by employee.id;`;
     db.query(sql, function (err, res) {
